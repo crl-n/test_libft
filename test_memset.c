692f6634c83a72765c0b44050189e31382cd72a8
cnysten <nysten.carl@gmail.com>
2021-11-08 14:56:38 +0200

    test_memset.c raw mem test added

--- test_memset.c
+++ test_memset.c
@@ -6,7 +6,7 @@
 /*   By: cnysten <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/01 16:21:22 by cnysten           #+#    #+#             */
-/*   Updated: 2021/11/08 13:02:57 by cnysten          ###   ########.fr       */
+/*   Updated: 2021/11/08 14:56:17 by cnysten          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@ static void	print_arr(int *arr, size_t n)
 	printf("\n");
 }
 */
-static void	test_adress_is_same(void *expected, void *actual, char *test_name)
+static void	test_adr(void *expected, void *actual, char *test_name)
 {
 	if (expected != actual)
 		print_failed(test_name);
@@ -33,9 +33,17 @@ static void	test_adress_is_same(void *expected, void *actual, char *test_name)
 }
 
 
-static void	test_string_is_same(void *expected, void *actual, char *test_name)
+static void	test_str(void *expected, void *actual, char *test_name)
 {
 	if (strcmp(expected, actual) != 0)
+		print_failed(test_name);
+	else
+		print_passed(test_name);
+}
+
+static void	test_mem(void *expected, void *actual, char *test_name)
+{
+	if (memcmp(expected, actual, (size_t)(sizeof (actual))) != 0)
 		print_failed(test_name);
 	else
 		print_passed(test_name);
@@ -44,34 +52,28 @@ static void	test_string_is_same(void *expected, void *actual, char *test_name)
 void	test_memset(void)
 {
 	print_ft("FT_MEMSET");
-	// Test if memset behaves like ft_memset
+
+	// Test if ft_memset behaves like memset with string
+	// and test if ft_memset returns the org pointer.
 	char	charArr[10] = "Hello.";
 	char	charArr2[10] = "Hello.";
-	char	*result = (char *) malloc(10 * sizeof (char));
-	if (!result)
-		return ;
-	result = ft_memset(charArr, 'x', 5);
-	printf("adr charArr: %p, adr res: %p\n", &charArr, &result);
-	printf("charArr: %s, result: %s\n", charArr, result);
-	test_string_is_same(result, memset(charArr2, 'x', 5), "\"Hello.\" to \"xxxxx.\"");
-	test_adress_is_same(charArr, result, "Pointers are the same");
+	char	*result = ft_memset(charArr, 'x', 5);
+	char	*result2 = memset(charArr2, 'x', 5);
+
+	test_str(result2, result, "\"Hello.\" to \"xxxxx.\"");
+	test_adr(charArr, result, "Pointers are the same");
+	printf("adr. in: %p, adr. out: %p\n", charArr, result);
+	printf("memset: %s, ft_memset: %s\n", result, result2);
+
+	// Test with null pointer
+	test_adr(memset(NULL, 0, 0), ft_memset(NULL, 0, 0), "Null returns null.");
+	printf("adr. in: %p, adr. out: %p\n", NULL, ft_memset(NULL, 0, 0));
+
+	// Test with raw memory and int array
+	int	intArr[] = {1, 2, 3};
+	int	intArr2[] = {1, 2, 3};
+	int	*intRes = ft_memset(intArr, 9, 3);
+	int	*intRes2 = memset(intArr2, 9, 3);
+
+	test_mem(intRes2, intRes, "123 to 999");
 }
-
-/*
-int	main(void)
-{
-	int	intArr[10];
-	int	intArr2[10];
-	ft_memset(intArr, 0, 10);
-
-	memset(intArr2, 0, 10);
-
-	print_arr(intArr, 10);
-	print_arr(intArr2, 10);
-
-	printf("%s\n", charArr);
-	printf("%s\n", charArr2);
-
-	return (0);
-}
-*/
