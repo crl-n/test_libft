e2600e7b2813082b7cc579f10d11055a7fdea1a3
cnysten <nysten.carl@gmail.com>
2021-11-08 14:57:32 +0200

    test_itoa.c memory leak fixed

--- test_itoa.c
+++ test_itoa.c
@@ -6,13 +6,11 @@
 /*   By: cnysten <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/07 17:19:47 by cnysten           #+#    #+#             */
-/*   Updated: 2021/11/08 11:00:41 by cnysten          ###   ########.fr       */
+/*   Updated: 2021/11/08 14:27:15 by cnysten          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
 #include "test_libft.h"
-#include <stdio.h>
 
 static void	test(char *expected, char *actual, char *test_name)
 {
@@ -25,5 +23,7 @@ static void	test(char *expected, char *actual, char *test_name)
 void	test_itoa(void)
 {
 	print_ft("FT_ITOA");
-	test("123", ft_itoa(123), "123");
+	char	*itoa = ft_itoa(123);
+	test("123", itoa, "123");
+	free(itoa);
 }
