5bf598216ea18bddcec96f2f6bed85731617700f
cnysten <nysten.carl@gmail.com>
2021-12-06 18:46:10 +0200

    test_striter.c unit test added

--- test_striter.c
+++ test_striter.c
@@ -1,10 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   test_striter.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cnysten <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/06 18:33:43 by cnysten           #+#    #+#             */
+/*   Updated: 2021/12/06 18:33:46 by cnysten          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "test_libft.h"
+
+static void	f(char *c)
+{
+	*c = 'b';
+}
 
 void	test_striter(void)
 {
 	t_test	*test = new_test();
+	char	s[] = "aaaa";
 
 	print_function("FT_STRITER");
+
+	ft_striter(s, f);
+	test_string("bbbb", s, test);
 
 	evaluate(test);
 	free(test);
