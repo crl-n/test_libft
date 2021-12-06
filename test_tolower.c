db925a5c11911924c39821095d88f3eee5c30f34
cnysten <nysten.carl@gmail.com>
2021-12-06 19:06:46 +0200

    test_tolower.c test fixed

--- test_tolower.c
+++ test_tolower.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   test_tolower.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cnysten <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/06 19:02:08 by cnysten           #+#    #+#             */
+/*   Updated: 2021/12/06 19:02:45 by cnysten          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "test_libft.h"
 
 void	test_tolower(void)
@@ -6,7 +18,7 @@ void	test_tolower(void)
 
 	print_function("FT_TOLOWER");
 
-	test_string("abc", ft_tolower("ABC"), test);
+	test_char('a', ft_tolower('A'), test);
 
 	evaluate(test);
 	free(test);
