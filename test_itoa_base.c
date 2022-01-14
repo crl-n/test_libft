bbc441c69c2eadc777cd0a5012a84a4afa9c7b98
Carl Nysten <nysten.carl@gmail.com>
2022-01-14 11:15:23 +0200

    test_itoa_base.c octal max int test

--- test_itoa_base.c
+++ test_itoa_base.c
@@ -3,8 +3,22 @@
 void	test_itoa_base(void)
 {
 	t_test	*test = new_test();
+	char	*s1 = NULL;
 
 	print_function("FT_ITOA_BASE");
+
+	s1 = ft_itoa_base(-2147483648, 10);
+	test_string("-2147483648", s1, test);
+	ft_strdel(&s1);
+
+	s1 = ft_itoa_base(0, 10);
+	test_string("0", s1, test);
+	ft_strdel(&s1);
+
+	s1 = ft_itoa_base(2147483647, 8);
+	if (!test_string("17777777777", s1, test))
+		printf("0x7fffffff, 8: %s\n", s1);
+	ft_strdel(&s1);
 
 	evaluate(test);
 	free(test);
